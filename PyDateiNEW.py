56837127496cb2eb9a5fb673b48b5fd237bfcbfd
nico-fst <108625504+nico-fst@users.noreply.github.com>
2022-09-25 19:12:51 +0200

    initial commit

---
+++ PyDateiNEW.py
@@ -0,0 +1,26 @@
+das hier behebt den testissue???
+#und dasd den zweiten automatisch
+
+def n2_wochenendeAmSee():
+    # dicts mit Einzukaufendem
+    alice = ["Bananen", "Brot", "Schokolade", "Rotwein"]
+    bob = ["Bier", "Bier", "Chips"]
+    eve = ["Brot", "Schokolade", "Chips", "Bier", "Wasser"]
+    personen = alice + bob + eve
+
+    # in dict Einkäufe unter Namen zählen
+    einkaufsliste = {}  # leeres dict erstellen
+    for element in personen:  # durch gesamte Liste mit Einkäufen iterieren
+        if element not in einkaufsliste:
+            einkaufsliste[element] = 1
+        else:
+            einkaufsliste[element] += 1
+
+    # Dict printen
+    print()
+    print("Einkaufsliste:")
+    print("-"*10)
+    for elements in einkaufsliste:
+        print("- " + elements + " x" + str(einkaufsliste[elements]))
+    print("-"*10)
+    print()
---
+++ README.md
@@ -0,0 +1,1 @@
+Github Tutorials auf YT ("by The Coding Train") folgende Übungen
